02102380a7c2e055c0817b55ba5446c031cc4ab1
Onyekachi Godswill <omenukooyekachi690@gmail.com>
2020-11-25 17:02:27 +0100

    Updates

---
+++ precache-manifest.41b4428b2d2b6bbb2b33e46e8a5d1250.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53be3fb307be5984bda8aa276bbb2c1",
+    "url": "/Shopping-cart-Task/index.html"
+  },
+  {
+    "revision": "d6392eb8ffb12421de29",
+    "url": "/Shopping-cart-Task/static/css/2.9db35e51.chunk.css"
+  },
+  {
+    "revision": "810f5302ea23d1647946",
+    "url": "/Shopping-cart-Task/static/css/main.c930ce4c.chunk.css"
+  },
+  {
+    "revision": "d6392eb8ffb12421de29",
+    "url": "/Shopping-cart-Task/static/js/2.523c5cdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Shopping-cart-Task/static/js/2.523c5cdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "810f5302ea23d1647946",
+    "url": "/Shopping-cart-Task/static/js/main.184f7abe.chunk.js"
+  },
+  {
+    "revision": "cad6f5d6a54d853ebae2",
+    "url": "/Shopping-cart-Task/static/js/runtime-main.bdef4188.js"
+  }
+]);
