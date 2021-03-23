7ca4da4df9597bb0b6be3ecd6764db313b765d03
Onyekachi Godswill <omenukooyekachi690@gmail.com>
2021-03-23 12:37:10 +0100

    Updates

---
+++ precache-manifest.df005b08df460530dc7cb22f36da0453.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4bad5461f5cca91b2ce66045657b46",
+    "url": "/Shopping-cart-Task/index.html"
+  },
+  {
+    "revision": "8fa856eb65c19a86ce64",
+    "url": "/Shopping-cart-Task/static/css/2.9db35e51.chunk.css"
+  },
+  {
+    "revision": "5285f347b7dbc40f3192",
+    "url": "/Shopping-cart-Task/static/css/main.c930ce4c.chunk.css"
+  },
+  {
+    "revision": "8fa856eb65c19a86ce64",
+    "url": "/Shopping-cart-Task/static/js/2.609106f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Shopping-cart-Task/static/js/2.609106f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5285f347b7dbc40f3192",
+    "url": "/Shopping-cart-Task/static/js/main.dc23ce81.chunk.js"
+  },
+  {
+    "revision": "cad6f5d6a54d853ebae2",
+    "url": "/Shopping-cart-Task/static/js/runtime-main.bdef4188.js"
+  },
+  {
+    "revision": "1883f29d4be880af49fde9fc10654597",
+    "url": "/Shopping-cart-Task/static/media/master-card.1883f29d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Shopping-cart-Task/precache-manifest.0f4188d20ea774b064685f3d221770b9.js"
+  "/Shopping-cart-Task/precache-manifest.df005b08df460530dc7cb22f36da0453.js"
 );
 
 self.addEventListener('message', (event) => {
